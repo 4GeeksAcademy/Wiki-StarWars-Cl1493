e9f1405dc61ef2078d68fd761fd228d84d0da4d8
Carla <152617155+Cl1493@users.noreply.github.com>
2024-04-01 18:46:16 +0000

    Navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-white mb-5 p-4">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src="https://www.teleadhesivo.com/es/img/as555-jpg/folder/products-listado-merchanthover/vinilos-decorativos-logo-star-wars-.jpg" width={50} height={50} className="photo"/>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<div class="btn-group" role="group">
+					<button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					Favorites
+					</button>
+					<ul class="dropdown-menu dropdown-menu-end p-3">
+					<li><a class="dropdown-item" href="#">Dropdown link</a></li>
+					<li><a class="dropdown-item" href="#">Dropdown link</a></li>
+					</ul>
+				</div>
 				</Link>
 			</div>
 		</nav>
