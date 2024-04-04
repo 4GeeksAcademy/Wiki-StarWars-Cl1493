ab210357fb5c0cec1c06869a54d444a81361b417
Carla <152617155+Cl1493@users.noreply.github.com>
2024-04-04 13:15:07 +0000

    Card de people

--- src/js/component/people.jsx
+++ src/js/component/people.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 
 
-export const Card = () => {
+export const People = (props) => {
+    const { store, actions } = useContext(Context);
 
 	return (
 		<div className="card" style="width: 18rem;">
             <img src="..." className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
                 <button type="button" className="btn btn-outline-warning">❤</button>
@@ -16,4 +21,9 @@ export const Card = () => {
 	);
 };
 
-export default Card;
+People.propTypes = {
+    title: PropTypes.string,
+    id: PropTypes.string,
+  };
+
+export default People;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Card } from "../component/card.jsx";
+import { People } from "../component/people.jsx";
 import { getState } from "../store/flux.js";
 
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Characters</h1>
+		<People />
+		<h1>Starships</h1>
 		<Card />
-		
+		<h1>Planets</h1>
+		<Card />
 	</div>
 );
